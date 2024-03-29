3872259cb0656cc30fcc417b2fb7fd062c57b650
m3na0 <m3na02@gmail.com>
2024-03-29 14:12:29 +0200

    fix the category

--- app/src/main/java/com/example/mymemo/Mood.java
+++ app/src/main/java/com/example/mymemo/Mood.java
@@ -1,4 +1,4 @@
-package com.example.mymemo.categoryscreens;
+package com.example.mymemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
--- app/src/main/java/com/example/mymemo/categoryscreens/CategoryActivity.java
+++ app/src/main/java/com/example/mymemo/categoryscreens/CategoryActivity.java
@@ -2,8 +2,11 @@ package com.example.mymemo.categoryscreens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.widget.Button;
+import com.example.mymemo.DiaryActivity;
+import com.example.mymemo.GalleryActivity;
 import com.example.mymemo.R;
 
 public class CategoryActivity extends AppCompatActivity {
@@ -12,5 +15,35 @@ public class CategoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
+
+        Button addBtn = findViewById(R.id.add_btn);
+        Button galleryBtn = findViewById(R.id.gallery_btn);
+        Button viewBtn = findViewById(R.id.view_btn);
+        Button viewBtn2 = findViewById(R.id.view_btn_2);
+
+
+        addBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(CategoryActivity.this, DiaryActivity.class);
+            startActivity(intent);
+
+        });
+
+        galleryBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(CategoryActivity.this, GalleryActivity.class);
+            startActivity(intent);
+        });
+
+        viewBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(CategoryActivity.this, Meetings.class);
+            startActivity(intent);
+        });
+
+        viewBtn2.setOnClickListener(v -> {
+            Intent intent = new Intent(CategoryActivity.this, Fitness.class);
+            startActivity(intent);
+        });
+
+
     }
+
 }
