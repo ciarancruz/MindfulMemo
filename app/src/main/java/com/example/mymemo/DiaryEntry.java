db1bbb7cc6d1e840c72f5d5226cad804cb05b319
Anmar <saoudanmar@gmail.com>
2024-04-10 13:15:31 +0200

    Added functionality for adding diaries

--- app/src/main/java/com/example/mymemo/DiaryEntry.java
+++ app/src/main/java/com/example/mymemo/DiaryEntry.java
@@ -3,6 +3,7 @@ package com.example.mymemo;
 import androidx.annotation.NonNull;
 import androidx.room.Entity;
 import androidx.room.ForeignKey;
+import androidx.room.Ignore;
 import androidx.room.Index;
 import androidx.room.Insert;
 import androidx.room.PrimaryKey;
@@ -23,44 +24,34 @@ import java.util.Date;
         indices = {@Index("user_id")}
 )
 public class DiaryEntry {
-    @PrimaryKey
-    @NonNull
+    @PrimaryKey(autoGenerate = true)
     int entry_id;
 
-    @NonNull
     @TypeConverters(DateConverter.class)
     Long date;
 
-    @NonNull
     String title;
 
-    @NonNull
     String text_content;
 
-    @NonNull
-    String entry_password;
-
-    @NonNull
     String video;
 
-    @NonNull
-    double lon;
-
-    @NonNull
-    double lat;
-
-    @NonNull
     int user_id;
 
-    public DiaryEntry(int entry_id,@NonNull Long date, @NonNull String title, @NonNull String text_content, @NonNull String entry_password, @NonNull String video, double lon, double lat) {
-        this.entry_id = entry_id;
+    @Ignore
+    public DiaryEntry(@NonNull Long date, String title, String text_content, String video) {
         this.date = date;
         this.title = title;
         this.text_content = text_content;
-        this.entry_password = entry_password;
         this.video = video;
-        this.lon = lon;
-        this.lat = lat;
+    }
+
+
+    public DiaryEntry(@NonNull Long date, String text_content, int user_id)
+    {
+        this.date = date;
+        this.text_content = text_content;
+        this.user_id = user_id;
     }
 
     public int getEntry_id() {
@@ -71,55 +62,28 @@ public class DiaryEntry {
         this.entry_id = entry_id;
     }
 
-    @NonNull
     public String getTitle() {
         return title;
     }
 
-    public void setTitle(@NonNull String title) {
+    public void setTitle(String title) {
         this.title = title;
     }
 
-    @NonNull
     public String getText_content() {
         return text_content;
     }
 
-    public void setText_content(@NonNull String text_content) {
+    public void setText_content(String text_content) {
         this.text_content = text_content;
     }
 
-    @NonNull
-    public String getEntry_password() {
-        return entry_password;
-    }
-
-    public void setEntry_password(@NonNull String entry_password) {
-        this.entry_password = entry_password;
-    }
-
-    @NonNull
     public String getVideo() {
         return video;
     }
 
-    public void setVideo(@NonNull String video) {
+    public void setVideo(String video) {
         this.video = video;
     }
 
-    public double getLon() {
-        return lon;
-    }
-
-    public void setLon(double lon) {
-        this.lon = lon;
-    }
-
-    public double getLat() {
-        return lat;
-    }
-
-    public void setLat(double lat) {
-        this.lat = lat;
-    }
 }
--- app/src/main/java/com/example/mymemo/diaryscreens/NewEntry.java
+++ app/src/main/java/com/example/mymemo/diaryscreens/NewEntry.java
@@ -1,17 +1,68 @@
 package com.example.mymemo.diaryscreens;
 
+import static androidx.core.content.ContentProviderCompat.requireContext;
+
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
+import com.example.mymemo.AppDatabase;
+import com.example.mymemo.DiaryEntry;
 import com.example.mymemo.R;
+import java.util.Calendar;
+
 
 public class NewEntry extends AppCompatActivity {
+    private AppDatabase db;
+    private EditText diaryEntry;
+    private TextView saveDiary;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entry);
+
+        db = Room.databaseBuilder(this, AppDatabase.class, "APP_DB")
+                .allowMainThreadQueries()
+                .build();
+
+        diaryEntry = findViewById(R.id.typetext);
+        saveDiary = findViewById(R.id.save_text);
+
+        saveDiary.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AddDiary();
+            }
+        });
+    }
+
+    private void AddDiary()
+    {
+        String diary = diaryEntry.getText().toString();
+
+        long currentTimeMillis = System.currentTimeMillis();
+        DiaryEntry diaryEntry = new DiaryEntry(currentTimeMillis, "sdhjfhjs", 1);
+        db.diaryEntryDao().insertDiaryEntry(diaryEntry);
+
+        Log.d("DiaryEntry", "Added diary to database");
+    }
+
+    class InsertAsyncDiary extends AsyncTask<DiaryEntry, Void, Void> {
+        @Override
+        protected Void doInBackground(DiaryEntry... DiaryEntry) {
+
+            AppDatabase.getInstance(getApplicationContext())
+                    .diaryEntryDao()
+                    .insertDiaryEntry(DiaryEntry[0]);
+            return null;
+        }
     }
 }
