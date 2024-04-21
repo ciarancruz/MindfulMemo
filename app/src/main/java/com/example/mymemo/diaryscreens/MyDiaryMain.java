06844c66fe4fbcae8ac538a28c69e62c66706e00
Anmar <saoudanmar@gmail.com>
2024-04-21 13:52:34 +0200

    Added search feature

--- app/src/main/java/com/example/mymemo/diaryscreens/MyDiaryMain.java
+++ app/src/main/java/com/example/mymemo/diaryscreens/MyDiaryMain.java
@@ -1,9 +1,11 @@
 package com.example.mymemo.diaryscreens;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -15,7 +17,10 @@ import com.example.mymemo.DiaryEntry;
 import com.example.mymemo.R;
 import android.content.Intent;
 import android.util.Log;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.View;
+import androidx.appcompat.widget.SearchView;
 import android.widget.Toast;
 
 import com.example.mymemo.AppDatabase;
@@ -24,12 +29,16 @@ import com.example.mymemo.ViewModal;
 import com.example.mymemo.HomeActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyDiaryMain extends AppCompatActivity {
     private User user;
     private DiaryAdapter adapter;
     private RecyclerView recyclerView;
     private AppDatabase db;
     private ViewModal viewModal;
+    private SearchView searchView;
 
     // User Model
     private int user_ID;
@@ -40,7 +49,24 @@ public class MyDiaryMain extends AppCompatActivity {
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavView);
         bottomNavigationView.setSelectedItemId(R.id.allDiary);
+        searchView = findViewById(R.id.search);
+        @SuppressLint("RestrictedApi") SearchView.SearchAutoComplete searchAutoComplete = searchView.findViewById(androidx.appcompat.R.id.search_src_text);
 
+        int textSizeInSp = 12;
+        searchAutoComplete.setTextSize(TypedValue.COMPLEX_UNIT_SP, textSizeInSp);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                filterList(newText);
+                return false;
+            }
+        });
 
         bottomNavigationView.setOnItemSelectedListener(item -> {
             int itemId = item.getItemId();
@@ -63,8 +89,6 @@ public class MyDiaryMain extends AppCompatActivity {
         db = Room.databaseBuilder(this, AppDatabase.class, "APP_DB")
                 .allowMainThreadQueries()
                 .build();
-
-        Log.d("Debug", "onCreate: Called");
 
         // Get user id
         Thread thread = new Thread(new Runnable() {
@@ -89,7 +113,7 @@ public class MyDiaryMain extends AppCompatActivity {
         viewModal = new ViewModelProvider(this).get(ViewModal.class);
 
 
-        // Deleting Recipes
+        // Deleting entries
         new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
             @Override
             public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
@@ -112,6 +136,35 @@ public class MyDiaryMain extends AppCompatActivity {
         }).attachToRecyclerView(recyclerView);
     }
 
+    private void filterList(String input) {
+        List<DiaryEntry> filteredList = new ArrayList<>();
+
+        try {
+            if (!input.isEmpty()) { // If user is searching
+                viewModal.getDiaryByUser(user_ID).observe(this, diaryEntries -> {
+                    filteredList.clear(); // Clear previous results
+                    for (DiaryEntry entry : diaryEntries) {
+                        if (entry.getTitle().toLowerCase().contains(input.toLowerCase())) {
+                            filteredList.add(entry);
+                        }
+                    }
+                    if (filteredList.isEmpty()) {
+                        Toast.makeText(this, "No matching diaries found", Toast.LENGTH_SHORT).show();
+                    }
+                    adapter.setDataList(filteredList);
+                });
+            } else { // If search input is empty, show all diaries related to the users id
+                viewModal.getDiaryByUser(user_ID).observe(this, diaryEntries -> {
+                    filteredList.clear(); // Clear previous results
+                    filteredList.addAll(diaryEntries);
+                    adapter.setDataList(filteredList);
+                });
+            }
+        } catch (Exception e) {
+            // Handle exceptions
+        }
+    }
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -121,10 +174,7 @@ public class MyDiaryMain extends AppCompatActivity {
             Log.d("Debug", "onCreate: diary "+ diaryEntries.size());
             adapter.setDataList(diaryEntries);
         });
-
     }
-
-
 
     public void navigateToHome(View view) {
         Intent intent = new Intent(this, HomeActivity.class);
